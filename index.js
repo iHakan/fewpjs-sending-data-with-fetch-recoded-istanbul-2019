54c39a8d9228e45fda4b9f55bc7e77b86bd60192
Hakan Yildiz <hakan@re-coded.com>
2020-04-24 18:11:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+window.addEventListener("loaded",function(e){
+  e.preventDefault();
+  
+})
+
+function submitData(userName,userEmail){
+  
+  let formData = {
+    name: userName,
+    email: userEmail
+  };
+  
+   let configObj = {
+    method:"POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+  
+ fetch('http://localhost:3000/users',configObj)
+  .then(response => response.json())
+  .then(data => handleRequest(data.users))
+  .catch(error => console.log(error));
+  
+  function handleRequest(data){
+    data.forEach(el => function(el){
+      const createEl = document.createEelement("div");
+      createEl.appendChild(el);
+    })
+  }
+}
+
